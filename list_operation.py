5dea854c79b48388fd7f6f589e323956359520d6
amen <amenteshomereg@gmail.com>
2025-11-29 19:09:57 -0800

    add script to calculate mean, max and sum

---
+++ list_operation.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+numbersList = [1, 2, 3, 4, 5, 6, 7]
+arrList = np.array(numbersList)
+
+
+print("Numbers List: ", numbersList)
+print("Array List: ", arrList)
+print("Mean: ", np.mean(arrList))
+print("Maximum: ", np.max(arrList))
+print("Sum: ", np.sum(arrList))
